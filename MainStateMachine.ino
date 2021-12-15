29b097bb3cabf16ab80bb10bb06986f7b3262bcf
15git-ruimendes <up201707154@fe.up.pt>
2021-12-15 14:06:07 +0000

    General Code Build

--- MainStateMachine.ino
+++ MainStateMachine.ino
@@ -1,10 +1,21 @@
+#include "Arduino.h"
 
-#define button 1
+#define BUTTON 1
+#define DIST_SENS 2
 
 int drill_X = 0;
 int drill_Y = 0;
+int state = 0;
+int trees = 0;
+long double distance = 0;
+char GCODE[256];
 
-int send_GCODE(char *GCODE)
+void setup()
+{
+    Serial.begin(9600);
+}
+
+int send_GCODE(char *buff)
 {
     bool success;
     if (success)
@@ -26,6 +37,14 @@ void create_Manipulator_GCODE(int x_axis, int y_axis, char *buff)
 {
 }
 
+void create_Magazine_GCODE(char *buff)
+{
+}
+
+void create_Sweeper_GCODE(char *buff)
+{
+}
+
 void display_LCD(int page_Number)
 {
 }
@@ -38,13 +57,14 @@ void update_Position()
 {
 }
 
-void setup()
+int get_NumberOfTrees()
 {
+}
 
-    int state = 0;
-    int trees = 0;
-    float distance = 0;
-    char GCODE[256];
+long double read_Distance()
+{
+    long double preVal = analogRead(DIST_SENS);
+    return 29.998 * pow(preVal, -1.173);
 }
 
 void loop()
@@ -53,7 +73,7 @@ void loop()
     {
         display_LCD(0);
     }
-    else if (state == 0 && (digitalRead(button)))
+    else if (state == 0 && (digitalRead(BUTTON)))
     {
         display_LCD(1);
         trees = get_NumberOfTrees();
@@ -63,5 +83,40 @@ void loop()
     {
         distance = read_Distance();
         create_Manipulator_GCODE(distance, 0, GCODE);
+        if (send_GCODE(GCODE))
+            state = 2;
+        else
+        {
+            display_LCD(3);
+            Serial.print("Communication error!!!!\n");
+        }
+    }
+    else if (trees == 0 && state == 1)
+    {
+        display_LCD(2);
+        Serial.print("No Trees Warning!!!!\n");
+        state = 0;
+    }
+    else if (state == 2)
+    {
+        create_Magazine_GCODE(GCODE);
+        if (send_GCODE(GCODE))
+            state = 3;
+        else
+        {
+            display_LCD(3);
+            Serial.print("Communication error!!!!\n");
+        }
+    }
+    else if (state == 3)
+    {
+        create_Sweeper_GCODE(GCODE);
+        if (send_GCODE(GCODE))
+            state = 3;
+        else
+        {
+            display_LCD(3);
+            Serial.print("Communication error!!!!\n");
+        }
     }
 }
