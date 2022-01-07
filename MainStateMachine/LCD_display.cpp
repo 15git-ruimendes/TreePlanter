59f428419b58da3f760a81afe08879db30babd6c
hpiqueiro <61315328+hpiqueiro@users.noreply.github.com>
2022-01-07 10:22:00 +0000

    Update LCD
    Página da calibração e emergência

--- MainStateMachine/LCD_display.cpp
+++ MainStateMachine/LCD_display.cpp
@@ -102,7 +102,7 @@ void display::page_2(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
         u8g.drawStr(81, 40, "Reload?");
         u8g.drawBox(80, 46, 48, 18);
         u8g.setColorIndex(0);
-        u8g.drawStr(87, 55, "Start?");
+        u8g.drawStr(87, 55, "Stop?");
         u8g.setColorIndex(1);
         u8g.setFont(u8g_font_unifont);
         u8g.drawStr(5, 30, "Moving");
@@ -123,7 +123,7 @@ void display::page_2(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
         u8g.drawStr(81, 40, "Reaload?");
         u8g.setColorIndex(1);
         u8g.drawFrame(80, 46, 48, 18);
-        u8g.drawStr(87, 55, "Start?");
+        u8g.drawStr(87, 55, "Stop?");
         u8g.setFont(u8g_font_unifont);
         u8g.drawStr(5, 30, "Moving");
         u8g.drawStr(5, 45, "Drill...");
@@ -151,7 +151,7 @@ void display::page_3(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
     u8g.drawStr(81, 40, "Reload?");
     u8g.setColorIndex(1);
     u8g.drawFrame(80, 46, 48, 18);
-    u8g.drawStr(87, 55, "Start?");
+    u8g.drawStr(87, 55, "Stop?");
     u8g.setFont(u8g_font_unifont);
     u8g.drawStr(5, 30, "Drilling");
     }
@@ -169,7 +169,7 @@ void display::page_3(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
     u8g.drawStr(81, 40, "Reload?");   
     u8g.drawBox(80, 46, 48, 18);
     u8g.setColorIndex(0);
-    u8g.drawStr(87, 55, "Start?");
+    u8g.drawStr(87, 55, "Stop?");
     u8g.setColorIndex(1);
     u8g.setFont(u8g_font_unifont);
     u8g.drawStr(5, 30, "Drilling");
@@ -191,17 +191,67 @@ void display::page_4(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
     u8g.setFont(u8g_font_6x10);
     u8g.drawStr(85, 10, "# Trees");
     u8g.setFont(u8g_font_6x10);
-    u8g.drawStr(100, 25, "10");
+    u8g.drawStr(100, 25, trees);
     u8g.drawBox(80, 28, 48, 18);
     u8g.setColorIndex(0);
     u8g.drawStr(81, 40, "Reload?");
     u8g.setColorIndex(1);
     u8g.drawFrame(80, 46, 48, 18);
-    u8g.drawStr(87, 55, "Start?");
+    u8g.drawStr(87, 55, "Stop?");
     u8g.setFont(u8g_font_unifont);
     u8g.drawStr(5, 30, "Planting");
     u8g.drawStr(5, 45, "The");
     u8g.drawStr(5, 60, "Tree");
+    }
+    else
+    {
+       u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(0, 10, "Tree Planter!");
+    u8g.drawLine(0, 15, 80, 15);
+    u8g.drawLine(80, 0, 80, 64);
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(85, 10, "# Trees");
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(100, 25, trees);
+    u8g.drawFrame(80, 28, 48, 18);
+    u8g.drawStr(81, 40, "Reload?");
+    u8g.drawBox(80, 46, 48, 18);
+    u8g.setColorIndex(0);
+    u8g.drawStr(87, 55, "Stop?");
+    u8g.setColorIndex(1);
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(5, 30, "Planting");
+    u8g.drawStr(5, 45, "The");
+    u8g.drawStr(5, 60, "Tree");
+    }
+}
+
+void display::page_5(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
+{
+    char trees[3];
+    trees[0] = num_trees / 10 + '0';
+    trees[1] = num_trees % 10 + '0';
+
+    if (top_page == 0)
+    {
+        u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(0, 10, "Tree Planter!");
+    u8g.drawLine(0, 15, 80, 15);
+    u8g.drawLine(80, 0, 80, 64);
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(85, 10, "# Trees");
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(100, 25, trees);
+    u8g.drawBox(80, 28, 48, 18);
+    u8g.setColorIndex(0);
+    u8g.drawStr(81, 40, "Reload?");
+    u8g.setColorIndex(1);
+    u8g.drawFrame(80, 46, 48, 18);
+    u8g.drawStr(87, 55, "Stop?");
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(5, 30, "Filling");
+    u8g.drawStr(5, 45, "With");
+    u8g.drawStr(5, 60, "Dirt");
     }
     else
     {
@@ -217,23 +267,20 @@ void display::page_4(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
     u8g.drawStr(81, 40, "Reload?");
     u8g.drawBox(80, 46, 48, 18);
     u8g.setColorIndex(0);
-    u8g.drawStr(87, 55, "Start?");
+    u8g.drawStr(87, 55, "Stop?");
     u8g.setColorIndex(1);
     u8g.setFont(u8g_font_unifont);
-    u8g.drawStr(5, 30, "Planting");
-    u8g.drawStr(5, 45, "The");
-    u8g.drawStr(5, 60, "Tree");
+    u8g.drawStr(5, 30, "Filling");
+    u8g.drawStr(5, 45, "With");
+    u8g.drawStr(5, 60, "Dirt");
     }
-}
-
-void display::page_5(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
-{
-    
 }
 
 void display::page_100(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
 {
-    u8g.setFont(u8g_font_6x10);
+    // pensei aqui colocar so uma frame a dizer Calibration
+
+    /*u8g.setFont(u8g_font_6x10);
     u8g.drawStr(0, 10, "Tree Planter!");
     u8g.drawLine(0, 15, 80, 15);
     u8g.drawLine(80, 0, 80, 64);
@@ -250,8 +297,71 @@ void display::page_100(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
     u8g.setFont(u8g_font_unifont);
     u8g.drawStr(5, 30, "Filling");
     u8g.drawStr(5, 45, "With");
-    u8g.drawStr(5, 60, "Dirt");
+    u8g.drawStr(5, 60, "Dirt");*/
+
+    char trees[3];
+    trees[0] = num_trees / 10 + '0';
+    trees[1] = num_trees % 10 + '0';
+
+    if (top_page == 0)
+    {
+        u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(0, 10, "Tree Planter!");
+    u8g.drawLine(0, 15, 80, 15);
+    u8g.drawLine(80, 0, 80, 64);
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(85, 10, "# Trees");
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(100, 25, trees);
+    u8g.setColorIndex(1);
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(5, 30, "Calibration...");
+    }
+    else
+    {
+       u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(0, 10, "Tree Planter!");
+    u8g.drawLine(0, 15, 80, 15);
+    u8g.drawLine(80, 0, 80, 64);
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(85, 10, "# Trees");
+    u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(100, 25, trees);
+    u8g.setColorIndex(1);
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(5, 30, "Calibration...");
+    }
+
 }
+
+void display::page_101(U8GLIB_ST7920_128X64_1X u8g, int top_page,int num_trees)
+{
+    // pensei aqui colocar só uma frame a dizer Emergência
+
+    // char trees[3];
+    // trees[0] = num_trees / 10 + '0';
+    // trees[1] = num_trees % 10 + '0';
+
+    if (top_page == 0)
+    {
+        u8g.setFont(u8g_font_6x10);
+        u8g.drawStr(50, 10, "Tree Planter!");
+        u8g.drawLine(0, 15, 100, 15);
+        u8g.setFont(u8g_font_unifont);
+        u8g.drawStr(40, 50, "EMERGENCY!");
+    }
+    else
+    {
+       u8g.setFont(u8g_font_6x10);
+    u8g.drawStr(50, 10, "Tree Planter!");
+    u8g.drawLine(0, 15, 100, 15);
+    u8g.setFont(u8g_font_unifont);
+    u8g.drawStr(40, 50, "EMERGENCY!");
+    }
+
+}
+
+
 
 void display::display_LCD(int page, U8GLIB_ST7920_128X64_1X u8g, int top_page, int num_trees)
 {
@@ -274,6 +384,9 @@ void display::display_LCD(int page, U8GLIB_ST7920_128X64_1X u8g, int top_page, i
         break;
     case 5:
         page_5(u8g, top_page, num_trees);
+        break;
+    case 101:
+        page_101(u8g, top_page, num_trees);
         break;
     default:
         page_100(u8g, top_page, num_trees);
