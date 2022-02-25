09521cdae489079fecae4bbcd711a14e762ad1ca
15git-ruimendes <60974905+15git-ruimendes@users.noreply.github.com>
2022-02-25 11:42:45 +0000

    Update LCD_test.ino

--- CodeSnippets/LCD/LCD_test/LCD_test.ino
+++ CodeSnippets/LCD/LCD_test/LCD_test.ino
@@ -1,17 +1,30 @@
-#include <SharpIR.h>
-#define Porta A1
-#define modelo 1080
-SharpIR detect(Porta, modelo);
+#include "U8glib.h"
+#include "LCD_Display.h"
+
+U8GLIB_ST7920_128X64_1X u8g(21,17,16);
+display lcd;
+
+int page[10] = {100,0,1,2,3,4,5,6,7,8,101};
+int trees = 8;
+int top_page = 1;
+
+int i = 0;
 
 void setup() {
-Serial.begin(9600);
-Serial.println("Capteur infrarouge Sharp 10-80 cm");
+  // put your setup code here, to run once:
+ lcd.setup_lcd(u8g);
 }
 
 void loop() {
- delay(100);
- float distance_now=detect.distance();
-Serial.print("distance  [cm]= ");
-Serial.println(distance_now);
- 
+  // put your main code here, to run repeatedly:
+  u8g.firstPage();
+  do{
+    lcd.display_LCD(page[i],u8g,top_page,trees);
+  }while(u8g.nextPage());
+  
+  delay(500);
+  if (i < 11)
+    i = 0;
+  else
+    i++;
 }
